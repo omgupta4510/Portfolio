1f6ab8e33b88acb581b137c98583c1cdaa8555da
omgupta4510 <omgupta4510@gmail.com>
2024-08-16 01:27:37 +0530

    commit

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -9,10 +9,10 @@ const Skills =()=>{
             <span>Apart from</span> <span> Development</span>
             </div>
             <div className="CPlink">
-            <Cp name="Codeforces" link="https://codeforces.com/profile/omgupta_4510" address="/code-forces.png"/>
-            <Cp name="Codechef" link="https://www.codechef.com/users/omgupta4510" address="/codechef.png"/>
+            <Cp name="Codeforces" link="https://codeforces.com/profile/omgupta_4510" address="code-forces.png"/>
+            <Cp name="Codechef" link="https://www.codechef.com/users/omgupta4510" address="codechef.png"/>
             <Cp name="Leetcode" link="https://leetcode.com/u/omgupta4510/" address="leetcode.png"/>
-            <Cp name="CSES" link="https://cses.fi/user/182981" address="/code.png"/>
+            <Cp name="CSES" link="https://cses.fi/user/182981" address="code.png"/>
             </div> 
             </div>
         </div>
